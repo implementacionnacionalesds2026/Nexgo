026ad296d70bec971fee4052a0b4897d16c6932b
Yeysoon <yeysonaldair13@gmail.com>
2026-04-20 00:10:59 -0600

    i18n: cambiar idioma de fechas a español

--- nexgo-frontend/src/app/app.config.ts
+++ nexgo-frontend/src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter, withViewTransitions }             from '@angular/router';
 import { provideHttpClient, withInterceptors }            from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import { routes }          from './app.routes';
 import { jwtInterceptor }  from './core/interceptors/jwt.interceptor';
+
+registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([jwtInterceptor])),
+    { provide: LOCALE_ID, useValue: 'es' },
   ],
 };
